cff9ed8b217d12dc39fae8caf762fe678751522a
oliprev <oliver.prevett@students.plymouth.ac.uk>
2025-01-08 23:37:43 +0000

    Set up Express server, with CORS as well as initial routing

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
 // Main backend code
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+const PORT = 9000;  
+const cors = require('cors');
+
+app.use(cors({ origin: 'http://localhost:3001' }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));

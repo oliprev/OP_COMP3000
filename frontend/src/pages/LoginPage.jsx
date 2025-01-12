880c6d8fbcce8aed6c813e8dbb3f13d25e928969
oliprev <oliver.prevett@students.plymouth.ac.uk>
2025-01-12 17:30:32 +0000

    Added axios import to LoginPage for backend API integration

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function LoginPage() {
     const [formData, setFormData] = React.useState({

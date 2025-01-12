5598bb9f20f1b91cd4e4d72d36d8b01ece7ef5aa
oliprev <oliver.prevett@students.plymouth.ac.uk>
2025-01-12 17:33:50 +0000

    Added MainPage component and update routing for protected access to MainPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import LandingPage from './pages/LandingPage'
 import RegisterPage from './pages/RegisterPage'
 import LoginPage from './pages/LoginPage'
+import MainPage from './pages/MainPage'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/main" element={<ProtectedRoute element ={MainPage} />} />
       </Routes>
     </Router>
   );

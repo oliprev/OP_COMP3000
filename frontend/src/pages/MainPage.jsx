bee90998a15e96eadec704376f1820fe698ac3a3
oliprev <oliver.prevett@students.plymouth.ac.uk>
2025-01-15 17:39:17 +0000

    Added navigation links to Profile and Log out on MainPage

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,11 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function MainPage() {
     return (
         <div>
-            
-            <h1>Glossary</h1>
-            <h1>Profile</h1>
+            <Link to = "/main/profile">Profile</Link><br></br>
+            <Link to = "/">Log out</Link><br></br>
         </div>
     );
 }

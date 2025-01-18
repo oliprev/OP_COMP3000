f6d30846645462499d8f17bb49bb4d57f5a5ed45
oliprev <oliver.prevett@students.plymouth.ac.uk>
2025-01-18 16:29:13 +0000

    Add dashboard heading to MainPage component

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 function MainPage() {
     return (
         <div>
+            <h1>Dashboard</h1>
             <Link to = "/main/profile">Profile</Link><br></br>
             <Link to = "/">Log out</Link><br></br>
         </div>

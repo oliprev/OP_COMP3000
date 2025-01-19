0a7f14c67262ac54f985f2ae0ebffc7a8d65a960
oliprev <oliver.prevett@students.plymouth.ac.uk>
2025-01-19 17:40:04 +0000

    Logout functionality fully implemented - removes token on logging out

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,10 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function MainPage() {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        localStorage.removeItem("userId");
+        navigate("/");
+    }
+    
     return (
         <div>
-            <Link to = "/" className="back-link">Log out</Link><br></br>
+            <Link to = "/" onClick={handleLogout} className="back-link">Log out</Link><br></br>
             <h1>Dashboard</h1>
             <Link to = "/main/modules">Training Modules</Link><br></br>
             <Link to = "/main/profile">Profile</Link><br></br>

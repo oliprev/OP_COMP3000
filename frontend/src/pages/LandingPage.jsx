98acfdf98ed9c71e6432708718a7d09ae158a954
oliprev <oliver.prevett@students.plymouth.ac.uk>
2025-02-05 17:33:31 +0000

    Added footer with developer attribution on LandingPage

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -7,6 +7,9 @@ function HomePage() {
             <h1>Untitled Cybersecurity Platform</h1>
             <Link to="/register">Register</Link><br></br> 
             <Link to="/login">Login</Link>
+            <footer>
+                <p>This application has been developed by Oliver Prevett, for the COMP3000 project.</p>
+            </footer>
         </div>
     );
 }

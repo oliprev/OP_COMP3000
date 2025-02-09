54fc3f15b6f6800b3bbaba991d1e4327b0107e81
oliprev <oliver.prevett@students.plymouth.ac.uk>
2025-02-09 15:02:49 +0000

    Updated schemata to reflect progress on organisation functionality

--- backend/models/User.js
+++ backend/models/User.js
@@ -6,8 +6,10 @@ const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true, maxlength: 254 }, // Email with max length of 254 characters
     password : { type: String, required: true }, // Password - hashed upon submission
     dateOfBirth : { type: Date, required: true }, // Date of birth
-    experienceLevel: { type: String, required: true, enum: ['Beginner', 'Intermediate', 'Advanced'] } // Experience level with enum values
-});
+    experienceLevel: { type: String, required: true, enum: ['Beginner', 'Intermediate', 'Advanced'] }, // Experience level with enum values
+    role: { type: String, enum: ['Admin', 'User'], default: 'User' }, // Role in organisation
+    organisation: { type: mongoose.Schema.Types.ObjectId, ref: 'Organisation', default: null } // Organisation (optional)
+}, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
 

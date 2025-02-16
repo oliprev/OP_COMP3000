de80fbcaf6361a81df48328625ba6d0c8c13d5da
oliprev <oliver.prevett@students.plymouth.ac.uk>
2025-02-16 16:39:57 +0000

    Removed code that was later added to ModuleDetailPage.jsx

--- frontend/src/pages/ModulePage.jsx
+++ frontend/src/pages/ModulePage.jsx
@@ -1,27 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import axios from "axios";
 
 function ModulePage() {
-    const [content, setContent] = useState({});
     const topics = ["Encryption", "Decryption", "Hashing", "Salting", "Key Exchange", "Digital Signatures", "Certificates", "Public Key Infrastructure", "Symmetric Key Encryption", "Asymmetric Key Encryption"];
-    
-    useEffect(() => {
-        const experienceLevel = localStorage.getItem("experienceLevel");
-        const fetchContent = async (topic) => {
-            try {
-                const response = await axios.get("http://localhost:9000/api/gemini/generate-content?topic=${topic}&experienceLevel=${experienceLevel}");
-                setContent(prevContent => ({ ...prevContent, [topic]: response.data.content }));
-            } catch (error) {
-                console.error("Error fetching content:", error);
-            }
-        }
-        
-        topics.forEach((topic) => {
-            fetchContent(topic);
-        });
-    }, [topics]);
-    
     
     return (
         <div>
@@ -31,12 +12,6 @@ function ModulePage() {
             {topics.map(topic => (
                 <div key={topic}>
                     <h3><Link to={`/main/modules/${topic}`}>{topic}</Link></h3>
-                    {content[topic] ? (
-                        <div>
-                            <h4>{content[topic].title}</h4>
-                            <p>{content[topic].description}</p>
-                        </div>
-                    ) : null}
                 </div>
             ))}
         </div>

bccbf8075eb9b01c8b8bb4465346f4048e34b31f
oliprev <oliver.prevett@students.plymouth.ac.uk>
2025-03-11 00:16:08 +0000

    New routes added - need further refining because code is a bit messy

--- backend/routes/cybokRoutes.js
+++ backend/routes/cybokRoutes.js
@@ -23,4 +23,41 @@ router.get('/knowledge-areas', async (req, res) => {
     }
 });
 
+router.get('/knowledge-areas/:id', async (req, res) => {
+    try {
+        const knowledgeArea = await KnowledgeArea.findById(req.params.id);
+        res.status(200).json(knowledgeArea);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+});
+
+router.get('/knowledge-areas/:id/subtopics', async (req, res) => {
+    try {
+        const knowledgeArea = await KnowledgeArea.findById(req.params.id);
+        res.json({
+            knowledgeArea: knowledgeArea.name,
+            subtopics: knowledgeArea.subtopics.map(subtopic => ({
+                _id: subtopic._id,
+                name: subtopic.name
+            }))
+        });
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+});
+
+router.get('/knowledge-areas/:id/subtopics/:subtopicId', async (req, res) => {
+    try {
+        const knowledgeArea = await KnowledgeArea.findOne({ "subtopics._id": req.params.subtopicId });
+        const subtopic = knowledgeArea.subtopics.find(subtopic => subtopic._id.toString() === req.params.subtopicId);
+        res.json({
+            subtopic: subtopic.name,
+            sections: subtopic.sections
+        });
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+});
+
 module.exports = router;

bd525e32c0627053b19acaf48d95ff6072283ec3
oliprev <oliver.prevett@students.plymouth.ac.uk>
2025-03-12 00:16:13 +0000

    Functionality added, making it work and move back to prior pages if needed

--- frontend/src/pages/LearningPage.jsx
+++ frontend/src/pages/LearningPage.jsx
@@ -1,5 +1,14 @@
+import { useParams, Link } from "react-router-dom";
+
 function LearningPage() {
-    return null;
-};
+    const { topic, subtopic } = useParams(); 
+
+    return (
+        <div>
+            <Link to={`/main/topics/${topic}/subtopics`} className="back-link">← Back to Subtopics</Link>
+            <h1>Learning Page</h1> 
+        </div>
+    );
+}
 
 export default LearningPage;

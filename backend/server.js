2c711e926ea7bd6643a0325b36e2a5f923e4a2fe
oliprev <oliver.prevett@students.plymouth.ac.uk>
2025-03-29 01:27:29 +0000

    API rate limiting introduced - reducing risk of misuse / DDoS

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const http = require('http');
+const rateLimit = require('express-rate-limit');
 const userRoutes = require('./routes/userRoutes'); // Import user routes
 const geminiRoutes = require('./routes/geminiRoutes'); // Import gemini routes
 const cybokRoutes = require('./routes/cybokRoutes'); // Import cybok routes
@@ -13,9 +14,15 @@ const cors = require('cors'); // Imports cors
 
 app.use(cors({ origin: 'http://localhost:3001' })); // Allows requests from frontend
 app.use(express.json()); // Allows JSON parsing
-app.use('/api/users', userRoutes); // Defines user route URL
-app.use('/api/gemini', geminiRoutes); // Defines gemini route URL
-app.use('/api/cybok', cybokRoutes); // Defines cybok route URL
+
+// Sets rate limiters for different routes
+const geminiLimiter = rateLimit({ windowMs: 60 * 1000, max: 20, message: 'You are being rate limited. Please try again later.' }); 
+const cybokLimiter = rateLimit({ windowMs: 60 * 1000, max: 25, message: 'You are being rate limited. Please try again later.' });
+const userLimiter = rateLimit({ windowMs: 60 * 1000, max: 25, message: 'You are being rate limited. Please try again later.' });
+
+app.use('/api/users', userLimiter, userRoutes); // Defines user route URL
+app.use('/api/gemini', geminiLimiter, geminiRoutes); // Defines gemini route URL
+app.use('/api/cybok', cybokLimiter, cybokRoutes); // Defines cybok route URL
 
 mongoose.connect(process.env.URI) // Connects to MongoDB
     .then(() => console.log('Connected to MongoDB')) // Logs success message

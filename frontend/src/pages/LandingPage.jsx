917232bf53532710315c7a92bdf99ca8f90d1d6f
oliprev <oliver.prevett@students.plymouth.ac.uk>
2025-03-31 13:09:36 +0100

    Livened LandingPage using MUI components - going to start using sx instead of style

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,16 +1,29 @@
+import { Container, Typography, Grid2, Card, CardActionArea, Box } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
 
 function HomePage() {
     return (
-        <div>
-            <h1>SecuLearn</h1>
-            <Link to = "/register">Register</Link><br></br> 
-            <Link to = "/login">Login</Link>
-            <footer>
-                <p>This application has been developed by Oliver Prevett, for the COMP3000 project.</p>
-            </footer>
-        </div>
+        <Container sx = {{ display: 'flex', flexDirection: 'column' }}>
+            <Typography variant = 'h1' fontWeight = '650'>SecuLearn</Typography>
+            <Grid2 container spacing = {4} justifyContent = {'center'} mt = {5} mb = {10}>
+                {[
+                    { label: "Register", path: "/register" },
+                    { label: "Login", path: "/login" },
+                ]. map((item) => (
+                    <Grid2 item md = {3} key = {item.label}>
+                        <Card>
+                            <CardActionArea component = {Link} to = {item.path} sx = {{ padding: 4 }}>
+                                <Typography variant = 'h4' fontWeight = {600}>{item.label}</Typography>
+                            </CardActionArea>
+                        </Card>
+                    </Grid2>
+                ))}
+            </Grid2>
+            <Box component = "footer" style = {{ py: 3 }}>
+                    <Typography variant = 'body1'>This application has been developed by Oliver Prevett, for the COMP3000 project.</Typography>
+            </Box>
+        </Container>
     );
 }
 
